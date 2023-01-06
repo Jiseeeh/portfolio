a9dea5309145c5db99590de5b801a6a7e186bd8f
Jiseeeh <johncarlo.camara1@gmail.com>
2023-01-06 20:50:34 +0800

    feat: add next-sanity

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+    SANITY_DATASET: process.env.SANITY_DATASET,
+    SANITY_API_VERSION: process.env.SANITY_API_VERSION,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
