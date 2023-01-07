8c5deb2cc039a23df006cf266235cd8fd6f4a54b
Jiseeeh <johncarlo.camara1@gmail.com>
2023-01-07 20:53:07 +0800

    feat: add hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 
+import Hero from "@feature/hero/Hero";
+
 export default function Home() {
   return (
     <>
@@ -9,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-5xl italic">HEY!</h1>
+      <Hero />
     </>
   );
 }
