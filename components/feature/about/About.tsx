e150d91b95f8ef8ff46982141f1e55503dcbffdb
Jiseeeh <johncarlo.camara1@gmail.com>
2023-01-08 21:59:02 +0800

    refractor: use extracted class

--- components/feature/about/About.tsx
+++ components/feature/about/About.tsx
@@ -7,7 +7,7 @@ interface AboutProps {
 const About: React.FC<AboutProps> = ({ data }) => {
   return (
     <article className="mb-6 max-w-6xl mx-auto ">
-      <h1 className="mb-5 text-5xl text-center font-bold">About me</h1>
+      <h1 className="mb-5 title text-center">About me</h1>
       <section className="m-3 p-6 rounded-md">
         <p className="p-2 leading-7">{data}</p>
       </section>
