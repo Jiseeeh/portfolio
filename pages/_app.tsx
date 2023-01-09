39afdd90ab752a02a08315eb7f9ba905e5cd7ea1
Jiseeeh <johncarlo.camara1@gmail.com>
2023-01-10 00:56:40 +0800

    feat: add react-hot-toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 import Navbar from "@feature/navbar/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Navbar>
-      <Component {...pageProps} />
-    </Navbar>
+    <>
+      <Toaster />
+      <Navbar>
+        <Component {...pageProps} />
+      </Navbar>
+    </>
   );
 }
