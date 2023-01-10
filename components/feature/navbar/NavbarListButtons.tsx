881572c09cc213d8f609c0442f32cc1dba5d7ef5
Jiseeeh <johncarlo.camara1@gmail.com>
2023-01-10 13:41:23 +0800

    feat: add links and on click

--- components/feature/navbar/NavbarListButtons.tsx
+++ components/feature/navbar/NavbarListButtons.tsx
@@ -7,22 +7,53 @@ interface NavbarListButtonsProps {
 const NavbarListButtons: React.FC<NavbarListButtonsProps> = ({
   unToggleSidebar,
 }) => {
+  const onLinkClick = () => {
+    if (unToggleSidebar) unToggleSidebar();
+  };
+
   return (
     <>
       <li>
-        <button className="btn btn-ghost rounded-lg">About me</button>
+        <a
+          href="#about"
+          className="btn btn-ghost rounded-lg"
+          onClick={onLinkClick}
+        >
+          About me
+        </a>
       </li>
       <li>
-        <button className="btn btn-ghost rounded-lg">Experience</button>
+        <a
+          href="#experience"
+          className="btn btn-ghost rounded-lg"
+          onClick={onLinkClick}
+        >
+          Experience
+        </a>
       </li>
       <li>
-        <button className="btn btn-ghost rounded-lg"> Skills</button>
+        <a
+          href="#skills"
+          className="btn btn-ghost rounded-lg"
+          onClick={onLinkClick}
+        >
+          {" "}
+          Skills
+        </a>
       </li>
       <li>
-        <button className="btn btn-ghost rounded-lg">Projects</button>
+        <a
+          href="#projects"
+          className="btn btn-ghost rounded-lg"
+          onClick={onLinkClick}
+        >
+          Projects
+        </a>
       </li>
       <li>
-        <button className="btn btn-ghost rounded-lg">Blog</button>
+        <a href="#" className="btn btn-ghost rounded-lg" onClick={onLinkClick}>
+          Blog
+        </a>
       </li>
     </>
   );
