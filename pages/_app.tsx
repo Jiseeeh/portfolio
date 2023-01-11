e39af4dc4606ac9a7fb5dd54a7bb0b99e54807c2
Jiseeeh <johncarlo.camara1@gmail.com>
2023-01-11 16:26:51 +0800

    feat: add anayltics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 import Navbar from "@feature/navbar/Navbar";
 
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Navbar>
         <Component {...pageProps} />
       </Navbar>
+      <Analytics />
     </>
   );
 }
