42beed7a2c7a37e1e477451c45e35fc22b6d66e6
Jiseeeh <johncarlo.camara1@gmail.com>
2023-01-30 22:10:02 +0800

    style: add width on md screen

--- components/feature/experience/NoWorkExperience.tsx
+++ components/feature/experience/NoWorkExperience.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const NoWorkExperience: React.FC = () => {
   return (
-    <section className="flex flex-col items-center mx-3 mb-20 gap-8 h-64 relative">
+    <section className="flex flex-col items-center relative mx-3 mb-20 gap-8 h-96 md:w-96">
       <h1 className="mt-5 text-2xl font-bold">Not yet working</h1>
       <Image
         className="object-contain"
